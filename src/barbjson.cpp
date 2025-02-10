30737f306fa66418d3fb834b07e83eabc9ab62b7
OmegaWei1984 <omegawei1984@gmail.com>
2025-02-10 23:53:59 +0800

    feat: parse 和 get_type

--- src/barbjson.cpp
+++ src/barbjson.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
-#include <string>
-
 #include "barbjson.hpp"
 
-std::string Barbjson::test() {
-    return "hello";
+using namespace Barb;
+
+ParseRet Json::parse(Value *v, const char *json) {
+    return ParseRet::kOk;
+}
+
+Type Json::get_type(const Value *v) {
+    return Type::kNull;
 }
